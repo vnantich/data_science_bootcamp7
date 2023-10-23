d0fa6168e027975bcd43b2902b09194d5dbbba08
vnantich <143310857+vnantich@users.noreply.github.com>
2023-10-23 17:26:16 +0700

    Update restaurantchatbot.R

--- R/restaurantchatbot.R
+++ R/restaurantchatbot.R
@@ -1,6 +1,4 @@
 reservation_bot <- function() {
-    name = readline("What's your name?: ")
-     print(paste("Hello", name))
      welcome = readline ("Welcome to Ichi Ni San Udon, your go-to place for homemade udon! How can we assist you today? 
                             1. I'd like to make a reservation.
                             2. I'd like to see the special menu of the month.
@@ -14,16 +12,16 @@ reservation_bot <- function() {
                 order = readline("Today, we have two specials: Spicy Beef Udon priced at 245 baht and Curry Chicken Udon priced at 200 baht. Which one would you like?: ")
                 }
                 else if (special == "No") {
-                    print ("Okay")
+                    cat ("Okay")
                     }
             allergies = readline ("Do you have any allergies we should be aware of?")
             celebration = readline ("Do you have any additional special requests or celebrations we should know about?")
+            name = readline("What's your name?: ")
             phone = readline ("To complete your reservation, we'll need your contact number. What's your phone number?")
-            print(paste("Thank you for choosing Ichi Ni San Udon! Your reservation for" ,datetime, "has been confirmed."))
+            cat(paste("Thank you for choosing Ichi Ni San Udon! Your reservation for" ,datetime, "has been confirmed."))
             }
         else if (welcome == "2"){
-                print("1. Spicy Beef Udon priced at 245 baht: Flank steak cut into cubes, served with our house-made spicy beef broth.
+                cat("1. Spicy Beef Udon priced at 245 baht: Flank steak cut into cubes, served with our house-made spicy beef broth.
                     2.Curry Chicken Udon priced at 200 baht: Chicken leg cut into cubes, along with potatoes and eggplant, served in a special curry soup.")
-
         }
 }
